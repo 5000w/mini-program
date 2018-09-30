27d29a53897e27f6ccc80d52441871ae7a10c533
zysallen <243816351@qq.com>
2018-09-30 10:34:23 +0800

    支付测试

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <button @click="pay">支付测试</button>
         <swiper :indicator-dots="true" :autoplay="true" :circular="true">
             <block v-for="(img,idx) in imgUrls" :key="idx">
                 <swiper-item>
@@ -12,26 +13,26 @@
                 <div>
                     <img src="/static/img/home-title-coupon.png" alt="" style="width: 22px;height: 16px;"> 专享优惠券
                 </div>
-                <span>领券购物更优惠</span>
-            </div>
-            <div class="coupon-list">
-                <div v-for="(item,idx) in coupons" class="coupon" :class="{disabled: item.state === 0}" :key="idx">
-                    {{ mapHomeCoupon[item.price] }}
+                    <span>领券购物更优惠</span>
+                </div>
+                <div class="coupon-list">
+                    <div v-for="(item,idx) in coupons" class="coupon" :class="{disabled: item.state === 0}" :key="idx">
+                        {{ mapHomeCoupon[item.price] }}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="commodity">
-            <div class="title">
-                <div>
-                    <img src="/static/img/home-title-hot.png" style="width: 17px;height: 19px;" alt=""> 热卖商品
+            <div class="commodity">
+                <div class="title">
+                    <div>
+                        <img src="/static/img/home-title-hot.png" style="width: 17px;height: 19px;" alt=""> 热卖商品
                 </div>
-                <span>热卖商品为您推荐</span>
+                        <span>热卖商品为您推荐</span>
+                    </div>
+                    <div class="goods-list">
+                        <Card v-for="(goods,idx) in goodsList" @click="handleClick" :key="idx" :src="goods.url || 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'" :name="goods.name" :price="goods.price"> </Card>
+                    </div>
+                </div>
             </div>
-            <div class="goods-list">
-                <Card  v-for="(goods,idx) in goodsList" @click="handleClick" :key="idx" :src="goods.url || 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'" :name="goods.name" :price="goods.price"> </Card>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script>
@@ -63,6 +64,28 @@
         },
 
     	methods: {
+            pay() {
+                this.fetch('payOrder',{price: 0.01})
+                .then(res => {
+                    wx.requestPayment({ 
+                        'timeStamp': res.timestamp,
+                        'nonceStr': res.nonceStr,
+                        'package': res.package,
+                        'signType': 'MD5',
+                        'paySign': res.paySign,
+                        'success': function (res) {
+                            wx.showToast({
+                                title: '支付成功',
+                                icon: 'success',
+                                duration: 2000              
+                            })            
+                        },
+                        'fail': function (res) {
+                            
+                        }
+                    })
+                })
+            },
             getCoupon() {
                 this.fetch('get_coupon').then(res => {
                     this.coupons = res.list
@@ -105,65 +128,65 @@
 </script>
 
 <style scoped lang="scss">
-    //跳过图床
-    $homeCouponBg: 'https://s1.ax1x.com/2018/09/19/ieweAK.png';
-    .disabled {
-        filter: grayscale(100%);
+//跳过图床
+$homeCouponBg: 'https://s1.ax1x.com/2018/09/19/ieweAK.png';
+.disabled {
+    filter: grayscale(100%);
+}
+.container {
+    background: #f6f6f6;
+}
+.slide-image {
+    width: 100vw;
+    height: 100%;
+}
+.title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 13px;
+    > span {
+        color: #b3b3b3;
     }
-    .container {
-    	background: #f6f6f6;
-    }
-    .slide-image {
-    	width: 100vw;
-    	height: 100%;
-    }
-    .title {
-    	display: flex;
-    	justify-content: space-between;
-    	align-items: center;
-    	font-size: 13px;
-    	>span {
-    		color: #b3b3b3;
-    	}
-        >div {
-            display: flex;
-            align-items: center;
-            >img {
-                margin-right: 10px;
-            }
+    > div {
+        display: flex;
+        align-items: center;
+        > img {
+            margin-right: 10px;
         }
     }
-    .coupon-wrapper,
-    .commodity {
-    	padding: 7px 13px;
-    	margin-top: 10px;
-    	background: white;
+}
+.coupon-wrapper,
+.commodity {
+    padding: 7px 13px;
+    margin-top: 10px;
+    background: white;
+}
+.coupon-list {
+    display: flex;
+    width: 100%;
+    overflow-x: auto;
+    margin-top: 10px;
+    .coupon {
+        width: 120px;
+        height: 45px;
+        background-image: url($homeCouponBg);
+        background-size: cover;
+        flex-shrink: 0;
+        margin: 0 8px;
+        color: #ec6941;
+        font-size: 13px;
+        text-align: right;
+        padding-right: 10px;
+        box-sizing: border-box;
+        line-height: 45px;
     }
-    .coupon-list {
-    	display: flex;
-    	width: 100%;
-    	overflow-x: auto;
-        margin-top: 10px;
-    	.coupon {
-    		width: 120px;
-    		height: 45px;
-            background-image: url($homeCouponBg);
-    		background-size: cover;
-    		flex-shrink: 0;
-    		margin: 0 8px;
-    		color: #ec6941;
-    		font-size: 13px;
-    		text-align: right;
-    		padding-right: 10px;
-    		box-sizing: border-box;
-    		line-height: 45px;
-    	}
-    }
-    .goods-list {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        padding: 0 5px;
-        margin-top: 15px;
-    }
+}
+.goods-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 5px;
+    margin-top: 15px;
+}
 </style>
